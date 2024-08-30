2d017ce42ad30a363b836ae70e42d38517e32b8c
dmschrein <darrelmschreiner@gmail.com>
2024-08-30 16:22:56 -0700

    fixed custom modal

--- src/components/global/custom-modal.tsx
+++ src/components/global/custom-modal.tsx
@@ -1,27 +1,28 @@
+'use client'
+import { useModal } from '@/providers/modal-provider'
+import React from 'react'
 import {
+  Dialog,
   DialogContent,
   DialogDescription,
-  DialogTitle,
-} from "@radix-ui/react-dialog";
-import { Dialog } from "@tremor/react";
-import React from "react";
-import { DialogHeader } from "../ui/dialog";
-import { useModal } from "@/providers/modal-provider";
+  DialogHeader,
+} from '../ui/dialog'
+import { DialogTitle } from '@radix-ui/react-dialog'
 
 type Props = {
-  title: string;
-  subheading: string;
-  children: React.ReactNode;
-  defaultOpen?: boolean;
-};
+  title: string
+  subheading: string
+  children: React.ReactNode
+  defaultOpen?: boolean
+}
 
-const CustomModal = ({ children, defaultOpen = false, subheading, title }: Props) => {
-  const { isOpen, setClose } = useModal();
+const CustomModal = ({ children, defaultOpen, subheading, title }: Props) => {
+  const { isOpen, setClose } = useModal()
   return (
-    <Dialog 
-      open={isOpen !== undefined ? isOpen : defaultOpen}
+    <Dialog
+      open={isOpen || defaultOpen}
       onOpenChange={setClose}
-      >
+    >
       <DialogContent className="overflow-scroll md:max-h-[700px] md:h-fit h-screen bg-card">
         <DialogHeader className="pt-8 text-left">
           <DialogTitle className="text-2xl font-bold">{title}</DialogTitle>
@@ -30,7 +31,7 @@ const CustomModal = ({ children, defaultOpen = false, subheading, title }: Props
         </DialogHeader>
       </DialogContent>
     </Dialog>
-  );
-};
+  )
+}
 
-export default CustomModal;
+export default CustomModal
