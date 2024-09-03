01ec75fa663e641d65764f8f74fc6272c263fb98
dmschrein <darrelmschreiner@gmail.com>
2024-09-03 09:21:23 -0700

    fixed dialog error

--- src/components/global/infobar.tsx
+++ src/components/global/infobar.tsx
@@ -1,14 +1,26 @@
 "use client";
 import { NotificationWithUser } from "@/lib/types";
 import { UserButton } from "@clerk/nextjs";
-import { Sheet, Bell } from "lucide-react";
 import React, { useState } from "react";
 import { twMerge } from "tailwind-merge";
-import { SheetTrigger } from "../ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import { Bell } from "lucide-react";
+import { Role } from "@prisma/client";
+import { Card } from "../ui/card";
+import { Switch } from "../ui/switch";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { ModeToggle } from "./mode-toggle";
 
 type Props = {
   notifications: NotificationWithUser | [];
-  role?: string;
+  role?: Role;
   className?: string;
   subAccountId?: string;
 };
@@ -16,6 +28,21 @@ type Props = {
 const InfoBar = ({ notifications, subAccountId, className, role }: Props) => {
   const [allNotifications, setAllNotifications] = useState(notifications);
   const [showAll, setShowAll] = useState(true);
+
+  const handleClick = () => {
+    if (!showAll) {
+      setAllNotifications(notifications);
+    } else {
+      if (notifications?.length !== 0) {
+        setAllNotifications(
+          notifications?.filter((item) => item.subAccountId === subAccountId) ??
+            []
+        );
+      }
+    }
+    setShowAll((prev) => !prev);
+  };
+
   return (
     <>
       <div
@@ -32,7 +59,63 @@ const InfoBar = ({ notifications, subAccountId, className, role }: Props) => {
                 <Bell size={17} />
               </div>
             </SheetTrigger>
+            <SheetContent className="mt-4 mr-4 pr-4 overflow-scroll">
+              <SheetHeader className="text-left">
+                <SheetTitle>Notifications</SheetTitle>
+                <SheetDescription>
+                  {(role === "AGENCY_ADMIN" || role === "AGENCY_OWNER") && (
+                    <Card className="flex items-center justify-between p-4">
+                      Current Subaccount
+                      <Switch onCheckedChange={handleClick} />
+                    </Card>
+                  )}
+                </SheetDescription>
+              </SheetHeader>
+              {allNotifications?.map((notification) => (
+                <div
+                  key={notification.id}
+                  className="flex flex-col gap-y-2 mb-2 overflow-x-scroll text-ellipsis"
+                >
+                  <div className="flex gap-2">
+                    <Avatar>
+                      <AvatarImage
+                        src={notification.User.avatarUrl}
+                        alt="Profile Picture"
+                      />
+                      <AvatarFallback className="bg-primary">
+                        {notification.User.name.slice(0, 2).toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="flex flex-col">
+                      <p>
+                        <span className="font-bold">
+                          {notification.notification.split("|")[0]}
+                        </span>
+                        <span className="text-muted-foreground">
+                          {notification.notification.split("|")[1]}
+                        </span>
+                        <span className="font-bold">
+                          {notification.notification.split("|")[2]}
+                        </span>
+                      </p>
+                      <small className="text-xs text-muted-foreground">
+                        {new Date(notification.createdAt).toLocaleDateString()}
+                      </small>
+                    </div>
+                  </div>
+                </div>
+              ))}
+              {allNotifications?.length === 0 && (
+                <div
+                  className="flex items-center justify-center text-muted-foreground"
+                  mb-4
+                >
+                  You have no notifications
+                </div>
+              )}
+            </SheetContent>
           </Sheet>
+          <ModeToggle />
         </div>
       </div>
     </>
